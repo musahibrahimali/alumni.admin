63a1cc2008cc546a2771f485643cdfb11290a7ee
musahibrahimali <musahibrahimali@gmail.com>
2021-12-28 22:39:10 +0000

    all done

--- components/PageChange/PageChange.js
+++ components/PageChange/PageChange.js
@@ -7,7 +7,7 @@ const PageChange = (props) => {
   return (
     <div>
       <div className="flex flex-col justify-center items-center bg-frosty-schene bg-cover bg-no-repeat h-screen">
-        <div className="bg-white dark:bg-gray-900 flex justify-center items-center py-4">
+        <div className="bg-transparent flex justify-center items-center py-4">
           <FacebookCircularProgress />
         </div>
         <h4 className="bg-gray-100 mt-4 px-8 bg-opacity-40 text-error-bg font-sans cursor-default font-bold text-lg md:text-2xl py-2">
